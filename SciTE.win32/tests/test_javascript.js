1e9580f5bc354ebc93d0399c6ff4595dc1a62f01
Thorsten Kani <marcedo@habmalnefrage.de>
2016-10-25 09:16:50 +0200

    use default charset setting

--- SciTE.win32/tests/test_javascript.js
+++ SciTE.win32/tests/test_javascript.js
@@ -1,14 +1,14 @@
 /*
- * .... Doc Comments seem to be okay.
+	*  .... Doc Comments seem to be okay.
  */
-
+ 
 // Check syntax Highlitening and multiline calltips.
 function nameMe(str){
 	$("#some_content").click (this, function() {
 		writeln("click_main_content");
-		var MyVeryLongVar = MyVeryLongVar + 20 - 0xA + 2;
+		var MyVeryLongVar = MyVeryLongVar + 20 - 0xA + 2; 
 		var win = window("Test").callbacks.add(cele);
-		str.replace (/ ^['"]/, "");
+		str.replace(/ ^['"]/, "");
 	})
 }
 
@@ -17,3 +17,4 @@ http = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
 http.Open("GET", "https://raw.githubusercontent.com/arjunae/myScite/master/.gitattributes", false);
 http.Send;
 WScript.Echo("http.StatusText:" + http.StatusText + "\nhttp.responseText:\n " + http.responseText);
+
