815b3cc0bee865700208cf4999ede1b3add20451
Thorsten Kani <marcedo@habmalnefrage.de>
2016-11-07 19:12:29 +0100

    fix Figref

--- SciTE.win32/Addons/tools/figref.cmd
+++ SciTE.win32/Addons/tools/figref.cmd
@@ -1,32 +1,32 @@
-@echo off 
+@echo off
 :: -------- Starter for SciTE ----------
-:: used to avoid Chaos and store  Language Distbins within their respective Directories. 
+:: used to avoid Chaos and store  Language Distbins within their respective Directories.
 :: ---------------------------------------
 
 set toolName=figref
 set toolExt=.cmd
-set toolParam=-f %toolPath%\small.flf %*
+set toolParam=-f %toolPath%\mini.flf %*
 
 ::~dp0 = Full Path to current Directory with trailing slash
 set toolPath=%~dp0%toolName%
 
-:: temporarly append toolsDir to local Path 
+:: temporarly append toolsDir to local Path
 set path=%path%;%toolPath%;
 :: first try if a user had installed a local package
 if exist %toolPath%\%toolName%%toolExt% (
 echo ~ WRapper: %toolName%%toolExt% %toolParam%
 call %toolPath%\%toolName%%toolExt% %toolParam%
 goto freude
-) 
+)
 
-:: then look for it within the system 
+:: then look for it within the system
 where /Q %toolName%%toolExt%
 
 IF %ERRORLEVEL% == 0 (
 echo ~ WRapper: %toolPath%\%toolName%%toolExt% %toolParam%
 where %toolName%%toolExt%
 %toolName%%toolExt% %toolParam%
-) else ( 
+) else (
 echo ... please install %toolName% or copy a custom pack to %toolPath% .
 )
 
