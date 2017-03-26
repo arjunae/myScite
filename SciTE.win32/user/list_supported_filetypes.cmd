95370b82b5a83d875ec50ee372452b703c4e06bc
Thorsten Kani <marcedo@habmalnefrage.de>
2017-03-26 13:04:22 +0200

    list_supported_filetypes

--- SciTE.win32/user/list_supported_filetypes.cmd
+++ SciTE.win32/user/list_supported_filetypes.cmd
@@ -1,16 +1,20 @@
 @echo off
 
+::
+:: 1) Export property "file.patterns" from all property files
+:: 2) Keep only base entries (which dont use references) 
+:: 3) Strip the filename prefixes which were added by FINDSTR
+::
+
 pushd %~dp0%
-if exist %tmp%\*.raw? del %tmp%\*.raw?
+if exist filetypes?.* del filetypes?.*
 
 :main
-REM 1) Export property "file.patterns" from all property files
+
 FINDSTR /SI "^file.patterns." *.properties > filetypes1.raw
 
-REM 2) Keep only base entries (which dont use references) 
 FINDSTR /SIV "$(" filetypes1.raw > filetypes2.raw
 
-REM 2) Strip the filename prefixes which were added by FINDSTR
 for /F "delims=: eol=# tokens=3" %%E in (filetypes2.raw) do (
  echo %%E>>filetypes.v150.txt
  echo %%E
