a06fa466a4f68903999729e5f7d8c3e8a7ee21c4
Thorsten Kani <marcedo@habmalnefrage.de>
2017-04-04 19:31:16 +0200

    sync git

--- contrib/3.7.4_lua5.3/SciTE_lua5.3.4/user/Addons/lua/SciTEStartup.lua
+++ contrib/3.7.4_lua5.3/SciTE_lua5.3.4/user/Addons/lua/SciTEStartup.lua
@@ -1,11 +1,14 @@
 -- Windows requires this for us to immediately see all lua output.
 io.stdout:setvbuf("no")
 
-defaultHome = props["SciteDefaultHome"]
+defaultHome = props["SciteDefaultHome"].."\\user"
 package.path =  package.path ..";"..defaultHome.."\\Addons\\?.lua;".. ";"..defaultHome.."\\Addons\\lua\\lua\\?.lua;"
 package.cpath = package.cpath .. ";"..defaultHome.."\\Addons\\lua\\c\\?.dll;"
 
 ---- SciTEStartup.lua gets called by extman, to ensure its available here.
+-- Load Sidebar (which uses "eventmanager.lua")
+--package.path = package.path .. ";"..defaultHome.."\\Addons\\lua\\mod-sidebar\\?.lua;"
+--dofile(props["SciteDefaultHome"]..'\\user\\Addons\\lua\\mod-sidebar\\URL_detect.lua')
 
 --print("Called StartupScript")
 --print(props['command.name.8.*'])
