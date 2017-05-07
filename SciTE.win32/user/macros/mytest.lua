8978e890182115035e0cdfe5d0ae45d4d7d410f7
Thorsten Kani <marcedo@habmalnefrage.de>
2017-05-07 21:05:16 +0200

    luacom listLinks sample

--- SciTE.win32/user/macros/mytest.lua
+++ SciTE.win32/user/macros/mytest.lua
@@ -35,7 +35,7 @@ function test_luaCom()
 	print("start Browser with CLSID "..sID)
 	oWeb = luacom.CreateObject("InternetExplorer.Application") 
 	assert(oWeb) 
-	oWeb:Navigate("http://www.freedos.org")
+	oWeb:Navigate("http://www.yahoo.com")
 	oWeb.Height =300
 	oWeb.Width= 500
 	oWeb.Visible = true
@@ -63,14 +63,25 @@ function _oWeb:DocumentComplete(oWin,url)
 end
 
 function siteParser(oWin)
-	-- ihtmlWindow contains our ihtmlDocument	
+	-- gets our IHTMLDocument. We can use all listed extensions from 1-8:
+	--	https://msdn.microsoft.com/en-us/library/ff975572(v=vs.85).aspx	
 	oDoc=oWin.Document
-	content=oDoc.head.innerhtml
-	-- using get / set prefix to access properties 
+	
+	-- using the oprional get / set prefix to access properties showed to be saver. 
 	title=oDoc:gettitle()
-	--gui.message(title)
 	oDoc:settitle("myTitle")
-		print(content)
+	
+	-- Now print out all links found within the Site
+	eTmp=oDoc.body:getElementsByTagName("a")
+	linksEnum=luacom.GetEnumerator(eTmp)
+	link=linksEnum:Next()
+	while link do
+		print("found webblink: "..link.href)
+		link=linksEnum:Next()
+	end
+	
+	--	content=oDoc.head.innerhtml
+	--	print(content)	
 	oDoc=nil
 end
 
