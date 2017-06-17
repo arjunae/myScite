31fd22086f8135d383aa3f34c9ba8835c6e70ee3
Thorsten Kani <marcedo@habmalnefrage.de>
2017-06-17 11:48:56 +0200

    Prepare myScite (June) RC1

--- SciTE.linux32/user/Addons/lua/SciTEStartup.lua
+++ SciTE.linux32/user/Addons/lua/SciTEStartup.lua
@@ -7,13 +7,6 @@ package.path = package.path ..";"..defaultHome.."/Addons/lua/lua/?.lua;".. ";"..
 package.path = package.path .. ";"..defaultHome.."/Addons/lua/mod-extman/?.lua;"
 package.cpath = package.cpath .. ";"..defaultHome.."/Addons/lua/c/?.so;"
 
---~ If available, use spawner-ex to help reduce flickering within scite_popen
-local pathSpawner= props["spawner.extension.path"]
-if not pathSpawner~="" then
- fnInit,err= package.loadlib(pathSpawner.."/spawner-ex.so",'luaopen_spawner')
- if not err then fnInit() end
-end
-
 --lua >=5.2.x renamed functions: 
 local unpack = table.unpack or unpack
 math.mod = math.fmod or math.mod
