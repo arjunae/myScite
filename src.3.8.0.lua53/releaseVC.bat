d0a368ea75564f41bd71d5a38912ba861509f4b0
Marcedo <marcedo@HabMalNeFrage.de>
2022-12-10 09:20:13 +0100

    If VS wasnt fond within PATH, search for it automatically

--- src.3.8.0.lua53/releaseVC.bat
+++ src.3.8.0.lua53/releaseVC.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion enableextensions
-set VCINSTALLDIR="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
+REM set VCINSTALLDIR="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
 SET buildContext=14.0
 SET arch=x86
 set BUILDTYPE=Release
@@ -26,18 +26,20 @@ if [%ERRORLEVEL%]==[2] (
 REM
 REM Start the Build
 REM
+echo.
+echo SciTE %BUILDTYPE% 
+echo Desired Target Architecture: %arch%
+REM init VS and search for it in case its not in systempath
+where vcvarsall.bat 1>NUL2>NUL
+if %ERRORLEVEL% EQU 1 (FOR /F "tokens=*" %%i IN ('where /r "c:\program files" vcvarsall.bat') DO call "%%i" %arch% ) else ( call vcvarsall.bat %arch% )
 REM Try to acquire a VisualStudio 14 Context
 REM If that fails, use systems highest available Version as defined via env var VS[xxx]COMNTOOLS
 REM SET arch=x64
-rem call forcevcversion.cmd %buildContext%
-if %errorlevel%==10 (
-echo please build myScite with VisualStudio Version greater or equal 2015
-goto en
-)
-echo.
-echo SciTE %BUILDTYPE% 
-echo Target Architecture will be: %arch%
-call %VCINSTALLDIR%\vcvarsall.bat  %arch%
+REM call forcevcversion.cmd %buildContext%
+REM if %errorlevel%==10 (
+REM echo please build myScite with VisualStudio Version greater or equal 2015
+REM goto en
+REM )
 if "%1"=="DEBUG" set parameter1=DEBUG=1
 echo.
 echo Compiling Scintilla
